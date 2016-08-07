4305af9849a0abba2bc3aee956c35ff4bbd0851a
Paul Emmerich <emmericp@net.in.tum.de>
2016-08-07 15:27:26 +0200

    add l2-forward example
    RSS seems to be broken at the moment

---
+++ examples/l2-forward.lua
@@ -0,0 +1,47 @@
+--- Forward packets between two ports
+local phobos   = require "phobos"
+local device   = require "device"
+local stats    = require "stats"
+local log      = require "log"
+local memory   = require "memory"
+local argparse = require "argparse"
+
+function master(...)
+	-- parse cli arguments
+	local parser = argparse()
+	parser:argument("dev", "Devices to use, specify the same device twice to echo packets."):args(2):convert(tonumber)
+	parser:option("-t --threads", "Number of threads per forwarding direction using RSS."):args(1):convert(tonumber):default(1)
+	local args = parser:parse(...)
+
+	-- configure devices
+	for i, dev in ipairs(args.dev) do
+		args.dev[i] = device.config{
+			port = dev,
+			txQueues = args.threads,
+			rxQueues = args.threads,
+			rssQueues = args.threads
+		}
+	end
+	device.waitForLinks()
+
+	-- start forwarding tasks
+	for i = 1, args.threads do
+		-- bidirectional fowarding
+		phobos.startTask("forward", args.dev[1]:getRxQueue(i - 1), args.dev[2]:getTxQueue(i - 1))
+		phobos.startTask("forward", args.dev[2]:getRxQueue(i - 1), args.dev[1]:getTxQueue(i - 1))
+	end
+	phobos.waitForTasks()
+end
+
+function forward(rxQueue, txQueue)
+	-- a bufArray is just a list of buffers that we will use for batched forwarding
+	local bufs = memory.bufArray()
+	while phobos.running() do -- check if Ctrl+c was pressed
+		-- receive one or more packets from the queue
+		local count = rxQueue:recv(bufs)
+		-- send out all received bufs on the other queue
+		-- the bufs are free'd implicitly by this function
+		txQueue:sendN(bufs, count)
+	end
+end
+
