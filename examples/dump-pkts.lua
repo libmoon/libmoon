1281921c1ff7749e5b200e3fb50f76839b7288fe
Paul Emmerich <p.emmerich@first-colo.net>
2016-08-09 22:28:58 +0200

    use ARP in packet dumper example

--- examples/dump-pkts.lua
+++ examples/dump-pkts.lua
@@ -4,10 +4,13 @@ local phobos = require "phobos"
 local device = require "device"
 local memory = require "memory"
 local arp    = require "proto.arp"
+local eth    = require "proto.ethernet"
 
 function master(rxPort)
-	local dev = device.config{port = rxPort}
+	local dev = device.config{port = rxPort, txQueues = 2}
 	device.waitForLinks()
+	arp.startArpTask{txQueue = dev:getTxQueue(1), ips = "172.17.0.20"}
+	arp.waitForStartup() -- race condition with arp.handlePacket() otherwise
 	phobos.startTask("dumper", dev:getRxQueue(0))
 	phobos.waitForTasks()
 end
@@ -19,8 +22,15 @@ function dumper(queue)
 		for i = 1, rx do
 			local buf = bufs[i]
 			buf:dump()
+			if buf:getEthernetPacket().eth:getType() == eth.TYPE_ARP then
+				-- inject arp packets to the ARP task
+				-- this is done this way instead of using filters to also dump ARP packets here
+				arp.handlePacket(buf)
+			else
+				-- do not free packets handlet by the ARP task, this is done by the arp task
+				buf:free()
+			end
 		end
-		bufs:free(rx)
 	end
 end
 
