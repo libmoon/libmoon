d68c76f29386e4ceb8275a404a8ee18e22f05de0
Paul Emmerich <emmericp@net.in.tum.de>
2016-08-13 20:07:01 +0200

    use proper inheritance for drivers

--- lua/drivers.lua
+++ lua/drivers.lua
@@ -7,14 +7,15 @@ mod.rte_i40e_pmd = require "driver.i40e"
 mod.rte_ixgbe_pmd = require "driver.ixgbe"
 mod.rte_virtio_pmd = require "driver.virtio"
 
--- this is a pretty stupid way to do "inheritance" (done this way due to issues with serialization)
--- it would probably be a good idea to change this to proper inheritance
 function mod.initDriver(dev)
+	local device = require "device"
 	local driver = mod[dev:getDriverName()]
 	if driver then
-		for k, v in pairs(driver) do
-			dev[k] = v
+		if not getmetatable(driver) then
+			driver.__index = driver
+			setmetatable(driver, device.__devicePrototype)
 		end
+		setmetatable(dev, driver)
 	end
 end
 
