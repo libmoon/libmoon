cfbf83455c8f5e282316a79eaa82ed507ac0ea65
Paul Emmerich <emmericp@net.in.tum.de>
2016-08-15 16:47:40 +0200

    run all tests in timestmaping example

--- examples/hardware-timestamping.lua
+++ examples/hardware-timestamping.lua
@@ -2,6 +2,7 @@
 
 local phobos = require "phobos"
 local device = require "device"
+local memory = require "memory"
 local ts     = require "timestamping"
 local hist   = require "histogram"
 local timer  = require "timer"
@@ -16,30 +17,36 @@ function configure(parser)
 end
 
 function master(args)
-	for i, dev in ipairs(args.dev) do
-		local dev = device.config{port = dev, txQueues = 1, rxQueues = 2}
-		args.dev[i] = dev
-	end
+	args.dev[1] = device.config{port = args.dev[1], txQueues = 2}
+	args.dev[2] = device.config{port = args.dev[2], rxQueues = 2}
 	device.waitForLinks()
-	local txQueue = args.dev[1]:getTxQueue(0)
+	local txQueue0 = args.dev[1]:getTxQueue(0)
+	local txQueue1 = args.dev[1]:getTxQueue(1)
 	local rxQueue0 = args.dev[2]:getRxQueue(0)
 	local rxQueue1 = args.dev[2]:getRxQueue(1)
-	--phobos.startTask("timestamper", txQueue, rxQueue0)
-	phobos.startTask("timestamper", txQueue, rxQueue0, 319)
-	--phobos.startTask("timestamper", txQueue, rxQueue0, 1234)
-	--phobos.startTask("timestamper", txQueue, rxQueue1)
-	--phobos.startTask("timestamper", txQueue, rxQueue1, 319)
+	phobos.startTask("timestamper", txQueue0, rxQueue0):wait()
+	phobos.startTask("timestamper", txQueue0, rxQueue1):wait()
+	phobos.startTask("timestamper", txQueue0, rxQueue0, 319):wait()
+	phobos.startTask("timestamper", txQueue0, rxQueue0, 1234):wait()
+	phobos.startTask("timestamper", txQueue0, rxQueue1, 319):wait()
+	phobos.startTask("timestamper", txQueue0, rxQueue1, 319, true):wait()
+	phobos.startTask("timestamper", txQueue0, rxQueue1, 319)
+	phobos.startTask("flooder", txQueue1, 319)
 	phobos.waitForTasks()
 end
 
-function timestamper(txQueue, rxQueue, udp)
+
+function timestamper(txQueue, rxQueue, udp, randomSrc)
 	local filter = rxQueue.qid ~= 0
 	log:info("Testing timestamping %s %s rx filtering for %d seconds.",
 		udp and "UDP packets to port " .. udp or "L2 PTP packets",
 		filter and "with" or "without",
 		RUN_TIME
 	)
-	local runtime = timer:new(5)
+	if randomSrc then
+		log:info("Using multiple flows, this can be slower on some NICs.")
+	end
+	local runtime = timer:new(RUN_TIME)
 	local hist = hist:new()
 	local timestamper
 	if udp then
@@ -47,23 +54,40 @@ function timestamper(txQueue, rxQueue, udp)
 	else
 		timestamper = ts:newTimestamper(txQueue, rxQueue)
 	end
-	if filter then
-		if udp then
-			rxQueue:filterUdpTimestamps()
-		else
-			rxQueue:filterL2Timestamps()
-		end
-	end
 	while phobos.running() and runtime:running() do
 		local lat = timestamper:measureLatency(function(buf)
 			if udp then
+				if randomSrc then
+					buf:getUdpPacket().udp:setSrcPort(math.random(1, 1000))
+				end
 				buf:getUdpPacket().udp:setDstPort(udp)
 			end
 		end)
-log:warn("Latency %d", lat or -1)
 		hist:update(lat)
 	end
 	hist:print()
+	if hist.numSamples == 0 then
+		log:error("Received no packets.")
+	end
 	print()
 end
 
+function flooder(queue, port)
+	log:info("Flooding link with UDP packets with the same flow 5-tuple.")
+	log:info("This tests whether the filter matches on payload.")
+	local mempool = memory.createMemPool(function(buf)
+		local pkt = buf:getUdpPtpPacket()
+		pkt:fill{
+			ethSrc = queue,
+		}
+		-- the filter should not match this
+		pkt.ptp:setVersion(0xFF)
+		pkt.udp:setDstPort(port)
+	end)
+	local bufs = mempool:bufArray()
+	local runtime = timer:new(RUN_TIME + 0.1)
+	while phobos.running() and runtime:running() do
+		bufs:alloc(60)
+		queue:send(bufs)
+	end
+end
