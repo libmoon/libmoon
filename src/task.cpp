9b840ddbad89e81323c1ec11320668587e34c2d3
Paul Emmerich <emmericp@net.in.tum.de>
2016-08-16 21:38:30 +0200

    detect correct path automatically

--- src/task.cpp
+++ src/task.cpp
@@ -2,6 +2,12 @@
 #include <cstring>
 #include <iostream>
 #include <memory>
+#include <string>
+#include <sstream>
+
+#include <unistd.h>
+#include <limits.h>
+#include <sys/stat.h>
 
 extern "C" {
 #include <lauxlib.h>
@@ -12,26 +18,75 @@ extern "C" {
 
 #include "task.hpp"
 
-#define MG_LUA_PATH "[[\
-lua/?.lua;\
-lua/?/init.lua;\
-lua/lib/?.lua;\
-lua/lib/?/init.lua;\
-../lua/?.lua;\
-../lua/?/init.lua;\
-../lua/lib/?.lua;\
-../lua/lib/?/init.lua;\
-../../lua/?.lua;\
-../../lua/?/init.lua;\
-../../lua/lib/?.lua;\
-../../lua/lib/?/init.lua;\
-]]"
-
 namespace phobos {
+
+	void find_base_dir_fail() {
+		std::cerr << "Could not find base dir" << std::endl;
+		std::abort();
+	}
+
+	bool is_base_dir(std::string const& path) {
+		struct stat buf;
+		// having one of these files in some random folder might be just co-incidence
+		// these are reasonable filenames for some phobos-related scripts...
+		// so we just check for both
+		return ::stat((path + "/lua/phobos.lua").c_str(), &buf) == 0
+		    && ::stat((path + "/lua/main.lua").c_str(), &buf) == 0;
+	}
+
+	// can't be done in Lua because we know nothing, not even where ljsyscall is
+	std::string find_base_dir() {
+		char buf[PATH_MAX];
+		if (::readlink("/proc/self/exe", buf, PATH_MAX) == -1) {
+			find_base_dir_fail();
+		}
+		std::string path(buf);
+		// check dir with the binary first
+		size_t dir_pos = path.find_last_of("/");
+		if (dir_pos == std::string::npos) {
+			find_base_dir_fail();
+		}
+		path = path.substr(0, dir_pos);
+		if (is_base_dir(path)) {
+			return path;
+		}
+		// check parent dir (where we'll find it when compiling in the build dir)
+		dir_pos = path.find_last_of("/");
+		if (dir_pos == std::string::npos) {
+			find_base_dir_fail();
+		}
+		path = path.substr(0, dir_pos);
+		if (is_base_dir(path)) {
+			return path;
+		}
+		// check cwd last
+		if (!getcwd(buf, PATH_MAX)) {
+			find_base_dir_fail();
+		}
+		path = buf;
+		if (is_base_dir(path)) {
+			return path;
+		}
+		find_base_dir_fail();
+		__builtin_unreachable();
+	}
+
+	std::string build_lua_path() {
+		std::string base = find_base_dir();
+		std::stringstream ss;
+		ss << "'";
+		ss << base << "/lua/?.lua;";
+		ss << base << "/lua/?/init.lua;";
+		ss << base << "/lua/lib/?.lua;";
+		ss << base << "/lua/lib/?/init.lua";
+		ss << "'";
+		return ss.str();
+	}
+
 	lua_State* launch_lua() {
 		lua_State* L = luaL_newstate();
 		luaL_openlibs(L);
-		luaL_dostring(L, "package.path = package.path .. ';' .. " MG_LUA_PATH);
+		luaL_dostring(L, (std::string("package.path = package.path .. ';' .. ") + build_lua_path()).c_str());
 		if (luaL_dostring(L, "require 'main'")) {
 			std::cerr << "Could not run main script: " << lua_tostring(L, -1) << std::endl;
 			std::abort();
