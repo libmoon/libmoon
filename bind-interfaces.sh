8da172ff222c4a6410378d9e6d41ccd190499cae
Paul Emmerich <emmericp@net.in.tum.de>
2016-08-27 23:56:45 +0200

    make path to DPDK customizable for embedding phobos

--- bind-interfaces.sh
+++ bind-interfaces.sh
@@ -14,7 +14,7 @@ do
 	then
 		echo "Found VirtIO NIC $id"
 		echo "Not binding VirtIO NIC to DPDK due to buggy activity detection in dpdk-devbind.py"
-		echo "Use deps/dpdk/tools/dpdk-devbind.py to bind VirtIO NICs manually"
+		echo "Use ${ERROR_MSG_SUBDIR}deps/dpdk/tools/dpdk-devbind.py to bind VirtIO NICs manually"
 	else
 		echo "Binding interface $id to DPDK"
 		tools/dpdk-devbind.py  --bind=igb_uio $id
@@ -26,7 +26,7 @@ if [[ $i == 0 ]]
 then
 	echo "Could not find any inactive interfaces to bind to DPDK. Note that this script does not bind interfaces that are in use by the OS."
 	echo "Delete IP addresses from interfaces you would like to use with Phobos and run this script again."
-	echo "You can also use the script dpdk-devbind.py in deps/dpdk/tools manually to manage interfaces used by Phobos and the OS."
+	echo "You can also use the script dpdk-devbind.py in ${ERROR_MSG_SUBDIR}deps/dpdk/tools manually to manage interfaces used by Phobos and the OS."
 	echo "VirtIO interfaces are also ignored due to the buggy activity detection in DPDK."
 fi
 
