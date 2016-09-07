7284cc1af69a31bd275264b868d9a81a372633f6
Dominik Scholz <scholzd@in.tum.de>
2016-09-08 00:22:15 +0200

    [device] Improved link status logging

--- lua/device.lua
+++ lua/device.lua
@@ -377,7 +377,12 @@ function dev:wait(maxWait)
 		end
 	until link.status
 	self.speed = link.speed
-	log:info("Device %d (%s) is %s: %s%s Mbit/s", self.id, self:getMacString(), link.status and "up" or "DOWN", link.duplexAutoneg and "" or link.duplex and "full-duplex " or "half-duplex ", link.speed)
+	local out = string.format("Device %d (%s) is %s: %s%s MBit/s", self.id, self:getMacString(), link.status and "up" or "DOWN", link.duplexAutoneg and "" or link.duplex and "full-duplex " or "half-duplex ", link.speed)
+	if link.status then
+		log:info(out)
+	else
+		log:error(out)
+	end
 	return link.status
 end
 
