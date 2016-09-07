600b0cce1d3e5ebf19b1a45477f9dcfdcd368319
Dominik Scholz <scholzd@in.tum.de>
2016-09-08 00:24:24 +0200

    [kni] Many updates, fixes and improvements

--- lua/kni.lua
+++ lua/kni.lua
@@ -1,6 +1,6 @@
-local ffi = require "ffi"
+local ffi 	= require "ffi"
 local dpdkc = require "dpdkc"
-local dpdk = require "dpdk"
+local dpdk 	= require "dpdk"
 require "utils"
 
 local mod = {}
@@ -12,7 +12,7 @@ mg_kni.__index = mg_kni
 ffi.cdef[[
 struct rte_kni;
 struct rte_kni * mg_create_kni(uint8_t port_id, uint8_t core_id, void* mempool_ptr, const char name[]);
-unsigned rte_kni_tx_burst 	( 	struct rte_kni *  	kni,
+unsigned mg_kni_tx_burst 	( 	struct rte_kni *  	kni,
 		struct rte_mbuf **  	mbufs,
 		unsigned  	num 
 	);
@@ -27,50 +27,59 @@ int rte_kni_release 	( 	struct rte_kni *  	kni	);
 void rte_kni_init(unsigned int max_kni_ifaces);
 ]]
 
-function mod.createKNI(core, device, mempool, name)
-  --printf("kni C pointer print:")
-  --printPtr(mempool)
-  core = core or 0
-  --printf("port id %d", device.id)
-  --printf("in KNI ptr memp %p", mempool)
-  --printPtr(mempool)
-  local kni = ffi.C.mg_create_kni(device.id, core, mempool, name)
-  --printPtr(mempool)
-  --printf("KNI should be nil = %p ,, mempool = %p", kni, mempool)
-  --if(kni == nil)then
-  --  printf("KNI == NIL !!!")
-  --else
-  --  printf("KNI not nil")
-  --end
-  return setmetatable({
-    kni = kni,
-    core = core,
-    device = device
-  }, mg_kni)
+-- only works with "insmod deps/dpdk/x86_64-native-linuxapp-gcc/kmod/rte_kni.ko"
+function mod.createKni(core, device, mempool, name)
+	core = core or 0
+	local kni = ffi.C.mg_create_kni(device.id, core, mempool, name)
+	return setmetatable({
+		kni = kni,
+		core = core,
+		device = device,
+		name = name
+	}, mg_kni)
 end
 
-function mg_kni:rxBurst(bufs, nmax)
-  return ffi.C.rte_kni_rx_burst(self.kni, bufs.array, nmax)
+-- not blocking recv
+function mg_kni:recv(bufs, nmax)
+	return ffi.C.rte_kni_rx_burst(self.kni, bufs.array, nmax)
 end
 
-function mg_kni:txSingle(mbuf)
-  ffi.C.mg_kni_tx_single(self.kni, mbuf)
+function mg_kni:sendN(bufs, nmax)
+	return ffi.C.mg_kni_tx_burst(self.kni, bufs.array, nmax)
+end
+
+function mg_kni:send(bufs)
+	return ffi.C.mg_kni_tx_burst(self.kni, bufs.array, bufs.size)
+end
+
+function mg_kni:sendSingle(mbuf)
+	ffi.C.mg_kni_tx_single(self.kni, mbuf)
 end
 
 function mg_kni:handleRequest()
-  ffi.C.rte_kni_handle_request(self.kni)
+	ffi.C.rte_kni_handle_request(self.kni)
+end
+
+function mg_kni:setIP(ip, net)
+	ip = ip or "192.168.1.1"
+	net = net or 24
+
+	-- TODO make this nicer
+	io.popen("/sbin/ifconfig " .. self.name .. " " .. ip .. "/" .. net)
+	self:handleRequest()	
+	dpdk.sleepMillisIdle(1)
 end
 
 function mg_kni:release()
-  return ffi.C.rte_kni_release(self.kni)
+	return ffi.C.rte_kni_release(self.kni)
 end
 
 function mod.init(num)
-  return ffi.C.rte_kni_init(num)
+	return ffi.C.rte_kni_init(num)
 end
 
 function mod.close()
-  ffi.C.rte_kni_close()
+ 	ffi.C.rte_kni_close()
 end
 
 return mod
