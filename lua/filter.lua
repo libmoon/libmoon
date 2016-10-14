79576a40da9e91babe90f1d8a548cf0710d69be4
Paul Emmerich <p.emmerich@first-colo.net>
2016-10-14 16:55:16 +0200

    port ntuple filters to new API

--- lua/filter.lua
+++ lua/filter.lua
@@ -101,6 +101,23 @@ struct rte_eth_l2_flow {
 	uint16_t ether_type;          /**< Ether type in big endian */
 };
 
+struct rte_eth_ntuple_filter {
+	uint16_t flags;
+	uint32_t dst_ip;
+	uint32_t dst_ip_mask;
+	uint32_t src_ip;
+	uint32_t src_ip_mask;
+	uint16_t dst_port;
+	uint16_t dst_port_mask;
+	uint16_t src_port;
+	uint16_t src_port_mask;
+	uint8_t proto;
+	uint8_t proto_mask;
+	uint8_t tcp_flags;
+	uint16_t priority;
+	uint16_t queue;
+};
+
 /**
  * A structure used to define the input for IPV4 flow
  */
@@ -470,180 +487,34 @@ function dev:dumpFilters()
 	C.fdir_get_infos(self.id)
 end
 
-
--- FIXME: port to DPDK 2.x
-ffi.cdef [[
-struct mg_5tuple_rule {
-    uint8_t proto;
-    uint32_t ip_src;
-    uint8_t ip_src_prefix;
-    uint32_t ip_dst;
-    uint8_t ip_dst_prefix;
-    uint16_t port_src;
-    uint16_t port_src_range;
-    uint16_t port_dst;
-    uint16_t port_dst_range;
-
-};
-
-struct rte_acl_ctx * mg_5tuple_create_filter(int socket_id, uint32_t num_rules);
-void mg_5tuple_destruct_filter(struct rte_acl_ctx * acl);
-int mg_5tuple_add_rule(struct rte_acl_ctx * acx, struct mg_5tuple_rule * mgrule, int32_t priority, uint32_t category_mask, uint32_t value);
-int mg_5tuple_build_filter(struct rte_acl_ctx * acx, uint32_t num_categories);
-int mg_5tuple_classify_burst(
-    struct rte_acl_ctx * acx,
-    struct rte_mbuf **pkts,
-    struct mg_bitmask* pkts_mask,
-    uint32_t num_categories,
-    uint32_t num_real_categories,
-    struct mg_bitmask** result_masks,
-    uint32_t ** result_entries
-    );
-uint32_t mg_5tuple_get_results_multiplier();
-]]
-
-local mg_filter_5tuple = {}
-mod.mg_filter_5tuple = mg_filter_5tuple
-mg_filter_5tuple.__index = mg_filter_5tuple
-
---- Creates a new 5tuple filter / packet classifier
---- @param socket optional (default: socket of calling thread), CPU socket, where memory for the filter should be allocated.
---- @param acx experimental use only. should be nil.
---- @param numCategories number of categories, this filter should support
---- @param maxNRules optional (default = 10), maximum number of rules.
---- @return a wrapper table for the created filter
-function mod.create5TupleFilter(socket, acx, numCategories, maxNRules)
-  socket = socket or select(2, dpdk.getCore())
-  maxNRules = maxNRules or 10
-
-  local category_multiplier = ffi.C.mg_5tuple_get_results_multiplier()
-
-  local rest = numCategories % category_multiplier
-
-  local numBlownCategories = numCategories
-  if(rest ~= 0) then
-    numBlownCategories = numCategories + category_multiplier - rest
-  end
-
-  local result =  setmetatable({
-    acx = acx or ffi.gc(ffi.C.mg_5tuple_create_filter(socket, maxNRules), function(self)
-      -- FIXME: why is destructor never called?
-      log:debug("5tuple garbage")
-      ffi.C.mg_5tuple_destruct_filter(self)
-    end),
-    built = false,
-    nrules = 0,
-    numCategories = numBlownCategories,
-    numRealCategories = numCategories,
-    out_masks = ffi.new("struct mg_bitmask*[?]", numCategories),
-    out_values = ffi.new("uint32_t*[?]", numCategories)
-  }, mg_filter_5tuple)
-
-  for i = 1,numCategories do
-    result.out_masks[i-1] = nil
-  end
-  return result
+--- Install a 5-tuple filter on the NIC.
+--- @param filter, a table containing the following fields
+---   all header fields default to 0 if not provided
+---   all masks to all bits set if a value is provided, to 0 otherwise
+---     srcIp, dstIp: IP addresses as strings
+---     srcIpMask, dstIpMask: IP address masks as strings
+---     srcPort, dstPort: L4 ports as numbers
+---     srcPortMask, dstPortMask: port masks
+---     proto: the L4 protocol id
+---     protoMask: protocol mask
+---     priority: the filter priority (see NIC specs for details), default = 1
+--- @param queue the target queue (id or queue object)
+function dev:fiveTupleFilter(filter, queue)
+	local filter = ffi.new("struct rte_eth_ntuple_filter", {
+		flags = 31, -- five-tuple
+		dst_ip = filter.dstIp and parseIPAddress(filter.dstIp) or 0,
+		dst_ip_mask = filter.dstIpMask and parseIPAddress(filter.dstIpMask) or filter.dstIp and 0xFFFFFFFF or 0,
+		src_ip = filter.srcIp and parseIPAddress(filter.srcIp) or 0,
+		src_ip_mask = filter.srcIpMask and parseIPAddress(filter.srcIpMask) or filter.srcIp and 0xFFFFFFFF or 0,
+		dst_port = filter.dstPort or 0,
+		dst_port_mask = filter.dstPortMask or filter.dstPort and 0xFFFF or 0,
+		src_port = filter.srcPort or 0,
+		src_port_mask = filter.srcPortMask or filter.srcPort and 0xFFFF or 0,
+		proto = filter.proto or 0,
+		proto_mask = filter.protoMask or filter.proto and 0xFF or 0,
+		priority = filter.priority or 1,
+		queue = type(queue) == "number" and queue or queue.qid
+	})
+	local err = C.rte_eth_dev_filter_ctrl(self.id, C.RTE_ETH_FILTER_NTUPLE, C.RTE_ETH_FILTER_ADD, filter)
+	return checkDpdkError(err, "setting 5tuple filter")
 end
-
-
---- Bind an array of result values to a filter category.
---- One array of values can be boun to multiple categories. After classification
---- it will contain mixed values of all categories it was bound to.
---- @param values Array of values to be bound to a category. May also be a number. In this case
----  a new Array will be allocated and bound to the specified category.
---- @param category optional (default = bind the specified array to all not yet bound categories),
----  The category the array should be bound to
---- @return the array, which was bound
-function mg_filter_5tuple:bindValuesToCategory(values, category)
-  if type(values) == "number" then
-    values = ffi.new("uint32_t[?]", values)
-  end
-  if not category then
-    -- bind bitmask to all categories, which do not yet have an associated bitmask
-    for i = 1,self.numRealCategories do
-      if (self.out_values[i-1] == nil) then
-        log:debug("Assigned default at category " .. tostring(i))
-        self.out_values[i-1] = values
-      end
-    end
-  else
-    log:debug("Assigned bitmask to category " .. tostring(category))
-    self.out_values[category-1] = values
-  end
-  return values
-end
-
---- Bind a BitMask to a filter category.
---- On Classification the corresponding bits in the bitmask are set, when a rule
---- matches a packet, for the corresponding category.
---- One Bitmask can be bound to multiple categories. The result will be a bitwise OR
---- of the Bitmasks, which would be filled for each category.
---- @param bitmask Bitmask to be bound to a category. May also be a number. In this case
----  a new BitMask will be allocated and bound to the specified category.
---- @param category optional (default = bind the specified bitmask to all not yet bound categories),
----  The category the bitmask should be bound to
---- @return the bitmask, which was bound
-function mg_filter_5tuple:bindBitmaskToCategory(bitmask, category)
-  if type(bitmask) == "number" then
-    bitmask = mbitmask.createBitMask(bitmask)
-  end
-  if not category then
-    -- bind bitmask to all categories, which do not yet have an associated bitmask
-    for i = 1,self.numRealCategories do
-      if (self.out_masks[i-1] == nil) then
-        log:debug("Assigned default at category " .. tostring(i))
-        self.out_masks[i-1] = bitmask.bitmask
-      end
-    end
-  else
-    log:debug("Assigned bitmask to category " .. tostring(category))
-    self.out_masks[category-1] = bitmask.bitmask
-  end
-  return bitmask
-end
-
-
---- Allocates memory for one 5 tuple rule
---- @return ctype object "struct mg_5tuple_rule"
-function mg_filter_5tuple:allocateRule()
-  return ffi.new("struct mg_5tuple_rule")
-end
-
---- Adds a rule to the filter
---- @param rule the rule to be added (ctype "struct mg_5tuple_rule")
---- @priority priority of the rule. Higher number -> higher priority
---- @category_mask bitmask for the categories, this rule should apply
---- @value 32bit integer value associated with this rule. Value is not allowed to be 0
-function mg_filter_5tuple:addRule(rule, priority, category_mask, value)
-  if(value == 0) then
-    log:fatal("Adding a rule with a 0 value is not allowed")
-  end
-  self.buit = false
-  return ffi.C.mg_5tuple_add_rule(self.acx, rule, priority, category_mask, value)
-end
-
---- Builds the filter with the currently added rules. Should be executed after adding rules
---- @param optional (default = number of Categories, set at 5tuple filter creation time) numCategories maximum number of categories, which are in use.
-function mg_filter_5tuple:build(numCategories)
-  numCategories = numCategories or self.numRealCategories
-  self.built = true
-  --self.numCategories = numCategories
-  return ffi.C.mg_5tuple_build_filter(self.acx, numCategories)
-end
-
---- Perform packet classification for a burst of packets
---- Will do memory violation, when Masks or Values are not correctly bound to categories.
---- @param pkts Array of mbufs. Mbufs should contain valid IPv4 packets with a
----  normal ethernet header (no VLAN tags). A L4 Protocol header has to be
----  present, to avoid reading at invalid memory address. -- FIXME: check if this is true
---- @param inMask bitMask, specifying on which packets the filter should be applied
---- @return 0 on successfull completion.
-function mg_filter_5tuple:classifyBurst(pkts, inMask)
-  if not self.built then
-    log:warn("New rules have been added without building the filter!")
-  end
-  --numCategories = numCategories or self.numCategories
-  return ffi.C.mg_5tuple_classify_burst(self.acx, pkts.array, inMask.bitmask, self.numCategories, self.numRealCategories, self.out_masks, self.out_values)
-end
-return mod
-
