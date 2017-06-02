2db253db815f6fbf1e345a8322f712d9945573b3
Paul Emmerich <emmericp@net.in.tum.de>
2017-06-02 13:46:20 +0200

    fix core affinity of shared tasks
    fixes MoonGen issue #176

--- lua/dpdk.lua
+++ lua/dpdk.lua
@@ -161,7 +161,7 @@ function mod.init()
 	argv[#argv + 1] = ("-c0x%08X%08X"):format(coreMaskUpper, coreMaskLower)
 	-- core mapping, shared cores use the highest IDs
 	local maxCore = cfg.cores[#cfg.cores]
-	local coreMapping = ("%d-%d,(%d-%d)@0"):format(cfg.cores[1], maxCore, maxCore + 1, maxCore + libmoon.config.numSharedCores)
+	local coreMapping = ("%d-%d,(%d-%d)@%d"):format(cfg.cores[1], maxCore, maxCore + 1, maxCore + libmoon.config.numSharedCores, cfg.cores[1])
 	argv[#argv + 1] = ("--lcores=%s"):format(coreMapping)
 
 	if cfg.pciBlacklist then
