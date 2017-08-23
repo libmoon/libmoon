a484984deefdda66c4db647497ba7c7bf8aa0b03
Paul Emmerich <emmericp@net.in.tum.de>
2017-08-23 16:25:35 +0200

    fix indentation

--- build.sh
+++ build.sh
@@ -10,35 +10,35 @@ MLX4=false
 MOON=false
 
 while :; do
-        case $1 in
-                -h|--help)
-                        echo "Usage: <no option> compile without Mellanox drivers; <-m|--mlx5> compile mlx5; <-n|--mlx4> compile mlx4; <-h|--help> help;"
-                        exit
-                        ;;
-                -m|--mlx5)
-                        echo "Build with mlx5 driver selected"
-                        OPTIONS="$OPTIONS""-DUSE_MLX5=ON "
-                        MLX5=true
+	case $1 in
+		-h|--help)
+			echo "Usage: <no option> compile without Mellanox drivers; <-m|--mlx5> compile mlx5; <-n|--mlx4> compile mlx4; <-h|--help> help;"
+			exit
+			;;
+		-m|--mlx5)
+			echo "Build with mlx5 driver selected"
+			OPTIONS="$OPTIONS""-DUSE_MLX5=ON "
+			MLX5=true
 			FLAGS="$FLAGS""--mlx5"
-                        ;;
-                -n|--mlx4)
-                        echo "Build with mlx4 driver selected"
-                        OPTIONS="$OPTIONS""-DUSE_MLX4=ON "
-                        MLX4=true
+			;;
+		-n|--mlx4)
+			echo "Build with mlx4 driver selected"
+			OPTIONS="$OPTIONS""-DUSE_MLX4=ON "
+			MLX4=true
 			FLAGS="$FLAGS""--mlx4"
-                        ;;
+			;;
 		--moongen) #For internal use only
 			echo "Build libmoon with MoonGen"
 			MOON=true
 			;;
-                -?*)
-                        printf 'WARN: Unknown option (abort): %s\n' "$1" >&2
-                        exit
-                        ;;
-                *)
-                        break
-        esac
-        shift
+		-?*)
+			printf 'WARN: Unknown option (abort): %s\n' "$1" >&2
+			exit
+			;;
+		*)
+			break
+	esac
+	shift
 done
 
 
@@ -60,10 +60,10 @@ cd deps/dpdk
 #build DPDK with the right configuration
 make config T=x86_64-native-linuxapp-gcc O=x86_64-native-linuxapp-gcc
 if ${MLX5} ; then
-        sed -ri 's,(MLX5_PMD=).*,\1y,' x86_64-native-linuxapp-gcc/.config
+	sed -ri 's,(MLX5_PMD=).*,\1y,' x86_64-native-linuxapp-gcc/.config
 fi
 if ${MLX4} ; then
-        sed -ri 's,(MLX4_PMD=).*,\1y,' x86_64-native-linuxapp-gcc/.config
+	sed -ri 's,(MLX4_PMD=).*,\1y,' x86_64-native-linuxapp-gcc/.config
 fi
 make -j $NUM_CPUS O=x86_64-native-linuxapp-gcc
 )
